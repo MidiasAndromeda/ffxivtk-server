dea7fcc2ef00fadf96b290d2511d39eaa8cfb878
Midias <midias.dev@gmail.com>
2019-08-28 20:09:44 +0200

    UserRouter example

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,29 @@ import * as morgan from 'morgan';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 
+// APP INIT
 const app = express();
+
+// Cors
 app.use(cors());
+// Logger
 app.use(morgan('dev'));
+//Json Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Routers
+const UsersRouter = express.Router();
+
+//Users Routes
+UsersRouter.route('/')
+  .get(async (req, res) => {
+    res.json("yes")
+  });
 
 app.get('/', (request, response) => {
   response.send('Hello world!');
 });
 
+app.use('/api/' + 'users', UsersRouter);
 app.listen(5000);
