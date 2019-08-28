016e666259e42567539eef3df24fa15fd6005fba
Midias <midias.dev@gmail.com>
2019-08-28 19:30:20 +0200

    EpressJs + Typescript init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import * as express from 'express';
+import * as morgan from 'morgan';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const app = express();
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (request, response) => {
+  response.send('Hello world!');
+});
+
+app.listen(5000);
--- src/users/interfaces/user.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface User extends Document {
-    id: string;
-    readonly name: string;
-}
